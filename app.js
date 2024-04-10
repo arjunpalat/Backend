fa657c430eba2b6f5da7d62efc68be32fdc96234
arjunpalat <palathinkaraarjun@gmail.com>
2024-04-10 12:59:55 +0530

    Update backend route for build

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ mongoose
   });
 
 app.use(cors());
-app.use(express.static("dist"));
+app.use(express.static("build"));
 app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ limit: "2mb", extended: true }));
 app.use(express.json());
@@ -31,9 +31,9 @@ app.use(middleware.requestLogger);
 
 app.use("/api", apiRouter);
 
-/* app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
-}); */
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
